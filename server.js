04114e4b4e69fde78c6794daa649c5e7c51fd0ac
William <110736589+ConnorMurphy9@users.noreply.github.com>
2022-10-26 16:14:40 -0400

    added get request to user routes

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const sequelize = require('./config/connection');
 const helpers = require('./utils/helpers')
 
 const app = express();
-const PORT = process.env.PORT || 3040;
+const PORT = process.env.PORT || 3001;
 
 //
 const sess = {
