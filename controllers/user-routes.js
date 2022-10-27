3b47776b4eae2e019d9d84416be19ee514b1c73a
William <110736589+ConnorMurphy9@users.noreply.github.com>
2022-10-27 13:18:47 -0400

    fixed merge error in user-routes

--- controllers/user-routes.js
+++ controllers/user-routes.js
@@ -48,4 +48,14 @@ router.delete('/:id', async (req, res) => {
     res.status(500).json(err);
 }});
 
+router.post('/', async (req, res) => {
+  // create a new tag
+  try {
+    const createdUserData = await User.create(req.body);
+    res.status(200).json(createdUserData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
 module.exports = router;
