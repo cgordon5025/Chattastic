6bb4edbceed4018ceece13f13f9f5fe67c5e9a6e
William <110736589+ConnorMurphy9@users.noreply.github.com>
2022-10-27 13:22:16 -0400

    fixed merge error in user-routes

--- public/js/script.js
+++ public/js/script.js
@@ -2,8 +2,7 @@ console.log("hello")
 
 let ChatEngine;
 ChatEngine = ChatEngineCore.create({
-publishKey: 'pub-c-1ec1a0aa-1745-4f40-bf2f-45021d89be5b',
-subscribeKey: 'sub-c-2032760e-5bfe-4054-81dd-5d9ea84edec6'
+
 })
 
 let newPerson = generatePerson(true);
