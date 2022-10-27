f91bf9ca6d4b10f51ada73de4412bbe5b9d9a7cd
NathDCoder <themusicemperor@gmail.com>
2022-10-27 19:42:44 -0400

    Login

--- public/js/login.js
+++ public/js/login.js
@@ -1,6 +1,51 @@
+
 const loginFormHandler = async (event) => {
 event.preventDefault();
   
 const userId = document.querySelector("#email-login").value.trim();
 const password = document.querySelector("#password-login").value.trim();
-}
+
+if (email && password) {
+    const reponse = await fetch("/api/users/login", {
+        method:"POST",
+        body: JSON.stringify({ email, password }),
+        headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+        document.location.replace("/");
+    } else {
+        alert("Failed login attempt.");
+    }
+  }  
+};
+
+const signupFormHandler = async (event) => {
+    event.preventDefault();
+
+    const username = document.querySelector("username-signup").value.trim();
+    const email = document.querySelector("#email-signup");
+    const password = document.querySelector('#password-signup').value.trim();
+
+    if (username && email && password) {
+        const reponse = await fetch("/controller/homeroutes", {
+            method: "POST",
+            body: JSON.stringify({ username, email, password}),
+            headers: { "Content-Type": "application/json"},
+        });
+
+        if (response.ok) {
+            document.location.replace("/");
+        } else {
+            alert("Failed sign up attempt.")
+        }
+    }
+};
+  
+document
+    .querySelector(".login-form")
+    .addEventListener("submit", loginFormHandler);
+
+document
+    .querySelector(".sugnup-form")
+    .addEventListener("submit", signupFormHandler);
