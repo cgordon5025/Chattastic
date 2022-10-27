01e9f885d1936e9725dc3d7eb514c4282b47a88b
Allison <farrella95@gmail.com>
2022-10-27 13:29:06 -0400

    post-delete

--- controllers/threadRoute.js
+++ controllers/threadRoute.js
@@ -33,4 +33,32 @@ router.get("/thread/:id", async (req, res) => {
   }
 });
 
+router.post("/", async (req, res) => {
+  // create a new tag
+  try {
+    const createdThread = await Thread.create(req.body);
+    res.status(200).json(createdThread);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  // delete one user by its `id` value
+  try {
+    const deletedThread = await Thread.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!deletedThread) {
+      res.status(404).json({ message: "No Thread found with this id!" });
+      return;
+    }
+    res.status(200).json(deletedThread);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
