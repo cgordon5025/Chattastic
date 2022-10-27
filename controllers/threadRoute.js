a24357a4a3c73467ff9f18a6b1c9d6725530acc2
Allison <farrella95@gmail.com>
2022-10-27 11:14:54 -0400

    thread

---
+++ controllers/threadRoute.js
@@ -0,0 +1,36 @@
+const router = require("express").Router();
+const Thread = require("../models/Thread");
+
+router.get("/", async (req, res) => {
+  try {
+    const thread = await Thread.findAll({
+      include: [
+        {
+          model: Thread,
+          attributes: "title",
+        },
+      ],
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.get("/thread/:id", async (req, res) => {
+  try {
+    const threadData = await Thread.findByPk(req.params.id, {
+      include: [
+        {
+          model: Thread,
+          attributes: ["id", "title"],
+        },
+      ],
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
