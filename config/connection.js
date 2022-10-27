4cb824789ec468373358ff32e7b36afae13f1697
Allison <farrella95@gmail.com>
2022-10-27 13:19:10 -0400

    index.js update

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,18 @@
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 let sequelize;
-require('dotenv').config();
+require("dotenv").config();
 if (process.env.JAWSDB_URL) {
-    sequelize = new Sequelize(process.env.JAWSDB_URL);
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-    sequelize = new Sequelize(
-        process.env.DB_NAME,
-        process.env.DB_USER,
-        process.env.DB_PASS,
-        {
-            host: "localhost",
-            dialect: "mysql",
-            port: 3306
-        }
-    );
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASS,
+    {
+      host: "localhost",
+      dialect: "mysql",
+      port: 3306,
+    }
+  );
 }
 module.exports = sequelize;
--- models/index.js
+++ models/index.js
@@ -1,18 +1,17 @@
 //we want to establish the connections and between the tables
-const User = require('./User');
-const ChatModel = require('./ChatModel')
+const User = require("./User");
+const Channel = require("./Channel");
+const Thread = require("./Thread");
 //Post to be added in later//
 // const Post = require('./Post');
 
+Channel.hasMany(Thread, {
+  foreignKey: "id",
+  onDelete: "CASCADE",
+});
 
-//User hasMany (insert table here)
-  User.hasMany(ChatModel, {
-    foreignKey: 'user_id',
-    onDelete: 'CASCADE'
-  });
+Thread.belongsTo(Channel, {
+  foreignKey: "id",
+});
 
-  ChatModel.belongsTo(User, {
-    foreignKey:'user_id'
-  })
-
-module.exports = {User, ChatModel};
+module.exports = { User, Channel, Thread };
