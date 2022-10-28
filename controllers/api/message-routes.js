43eff35758f5d51e36f8192055f0d566f5639d1b
Abigail LeConte <abigailleconte@Abigails-MBP.attlocal.net>
2022-10-28 14:18:11 -0400

    created message routes, js files

---
+++ controllers/api/message-routes.js
@@ -0,0 +1,66 @@
+const router = require("express").Router();
+const Message = require("../../models");
+
+router.get("/", async (req, res) => {
+  try {
+    const message = await Message.findAll({
+      include: [
+        {
+          model: Message,
+          attributes: "text",
+        },
+      ],
+    });
+    res.status(200).json(message)
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.get("/Message/:id", async (req, res) => {
+  try {
+    const messageData = await Message.findByPk(req.params.id, {
+      include: [
+        {
+          model: Message,
+          attributes: ["text"],
+        },
+      ],
+    });
+    res.status(200).json(messageData)
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.post("/", async (req, res) => {
+  // create a new message
+  try {
+    const createdMessage = await Message.create(req.body);
+    res.status(200).json(createdMessage);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  // delete one message by its `id` value
+  try {
+    const deleteMessage = await Message.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!deleteMessage) {
+      res.status(404).json({ message: "No Thread found with this id!" });
+      return;
+    }
+    res.status(200).json(deleteMessage);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
--- controllers/user-routes.js
+++ controllers/user-routes.js
@@ -1,12 +1,12 @@
 const router = require("express").Router();
 const { User, Post } = require("../models");
 
-// get all users
+// get all messages
 router.get("/", async (req, res) => {
-  // find all users
+  // find all users --- I think I might need to add more code to the findAll {}
   try {
-    const allUserData = await User.findAll({ include: Post });
-    res.status(200).json(allUserData);
+    const allMessageData = await Message.findAll({ include: user_id }); 
+    res.status(200).json(allMessageData);
   } catch (err) {
     res.status(500).json(err);
   }
@@ -18,11 +18,11 @@ router.get("/:id", async (req, res) => {
   try {
     const userIdData = await User.findByPk(req.params.id, {
       // JOIN with Post
-      include: [{ model: Post }]
+      include: [{ model: Message }]
     });
 
     if (!userIdData) {
-      res.status(404).json({ message: "No user found with this id!" });
+      res.status(404).json({ message: "No messages found for this user!" });
       return;
     }
   } catch (err) {
@@ -30,68 +30,6 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-//this needs to be post since we don't want to remove the individual from the  database, just end their session
-router.post('/logout', async (req, res) => {
-  try {
-    if (req.session.loggedIn) {
-      req.session.destroy(() => {
-        res.status(204).end()
-      });
-    };
-    if (!deletedUserData) {
-      res.status(404).json({ message: 'No user found with this id!' });
-      return;
-    }
-    res.status(200).json(deletedUserData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
 
-router.post('/login', async (req, res) => {
-  try {
-    const userData = await User.findOne({ where: { username: req.body.username } });
-    if (!userData) {
-      res.status(404).json({ message: "Login failed please try again!" });
-      return;
-    }
-    const validPassword = await userData.checkPassword(req.body.password);
-
-    if (!validPassword) {
-      res.status(404).json({ message: "Incorrect password, please try again" });
-      return;
-    }
-    req.session.save(() => {
-      req.session.userID = userData.id;
-      req.session.username = userData.username;
-      req.session.loggedIn = true;
-      res.status(200).json({ message: "you are now logged in! YAY" })
-    });
-    console.log(req.session.userID)
-
-  } catch (err) {
-    res.status(500).json(err)
-  }
-})
-
-router.post('/signup', async (req, res) => {
-  try {
-    const createdUserData = await User.create({
-      username: req.body.username,
-      email: req.body.email,
-      password: req.body.password,
-    });
-    //this sets up the new user information
-    req.session.save(() => {
-      req.session.loggedIn = true;
-      req.session.userID = userData.id;
-      req.session.username = userData.username;
-      res.status(200).json(userData)
-    });
-    res.status(200).json(createdUserData);
-  } catch (err) {
-    res.status(400).json(err);
-  }
-});
 
 module.exports = router;
