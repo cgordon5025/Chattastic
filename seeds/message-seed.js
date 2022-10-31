404fef0f9a5ce4519355a186498f2970b9d01d75
Charlee Gordon <cgordon5025@gmail.com>
2022-10-31 19:26:43 -0400

    final push for the night

--- seeds/message-seed.js
+++ seeds/message-seed.js
@@ -2,13 +2,13 @@ const { Message } = require('../models')
 
 const messageData = [
     {
-        text: "helllllllllo",
+        text: "Welcome to the Site!",
         user_id: 1,
         thread_id: 1,
         channel_id: 1
     },
     {
-        text: "why hello to you too",
+        text: "Good to have you here!",
         user_id: 2,
         thread_id: 1,
         channel_id: 1
